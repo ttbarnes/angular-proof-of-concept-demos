1583e3e4931072e647c208f9ec996c3ce76256b1
ttbarnes <tony@tonybarnes.me>
2015-03-19 15:17:55 +0000

    unit tests for active checkbox label

--- test/spec/controllers/active-checkbox-label.js
+++ test/spec/controllers/active-checkbox-label.js
@@ -1,22 +1,42 @@
 'use strict';
 
-describe('Controller: AboutCtrl', function () {
+describe('Controller: activeCheckboxLabelCtrl', function () {
 
   // load the controller's module
   beforeEach(module('ajsRouterSeperationApp'));
 
-  var AboutCtrl,
+  var activeCheckboxLabelCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    AboutCtrl = $controller('activeCheckboxLabelCtrl', {
+    activeCheckboxLabelCtrl = $controller('activeCheckboxLabelCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should have 3 items defined', function () {
+    expect(scope.items.length).toBe(3);
   });
+
+  it('should have correct copy in the item 1', function () {
+    expect(scope.items[0].title).toEqual('item 1');
+    expect(scope.items[0].description).toEqual('description 1');
+  });
+
+  it('should have correct copy in the item 2', function () {
+    expect(scope.items[1].title).toEqual('item 2');
+    expect(scope.items[1].description).toEqual('description 2');
+  });
+
+  it('should have correct copy in the item 3', function () {
+    expect(scope.items[2].title).toEqual('item 3');
+    expect(scope.items[2].description).toEqual('description 3');
+  });
+
+  it('should have a false isChecked variable', function(){
+    expect(scope.isChecked).toEqual(false);
+  });
+
 });
