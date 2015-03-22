b3131661942923cbc5127333a7ac0fa7226a742e
ttbarnes <tony@tonybarnes.me>
2015-03-22 16:58:44 +0000

    nested ng-repeat data, template

--- app/scripts/controllers/nested-ng-repeat.js
+++ app/scripts/controllers/nested-ng-repeat.js
@@ -5,13 +5,34 @@ angular.module('proofOfConceptDemos')
 
     $scope.fruits = [
       {
-        name: 'apple'
+        name: 'apple',
+        info:[{
+          color: 'various',
+          size: 'small',
+          additionalInfo:[{
+            purchase:'singular or in bunches'
+          }]
+        }]
       },
       {
-        name: 'banana'
+        name: 'banana',
+        info:[{
+          color: 'yellow or green',
+          size: 'small-medium',
+          additionalInfo:[{
+            purchase:'singular or in bunches'
+          }]
+        }]
       },
       {
-        name: 'pineapple'
+        name: 'pineapple',
+        info:[{
+          color: 'yellow or green',
+          size: 'medium',
+          additionalInfo:[{
+            purchase:'singular'
+          }]
+        }]
       }
 
     ];
