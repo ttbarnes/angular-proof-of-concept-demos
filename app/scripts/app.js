36758b367c5e414d7adc1b40de0cb0ecae3a2997
ttbarnes <tony@tonybarnes.me>
2015-03-26 19:05:32 +0000

    scroll to top on viewContentLoaded

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,5 +17,10 @@ angular
     function ($rootScope, $state, $stateParams) {
       $rootScope.$state = $state;
       $rootScope.$stateParams = $stateParams;
+
+      $rootScope.$on('$viewContentLoaded', function(){ 
+        window.scrollTo(0, 0); 
+      });
+
     }
   ]);
