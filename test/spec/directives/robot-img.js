107d21350ca1b089c3b65f1cb66a0c0042bbe300
ttbarnes <tony@tonybarnes.me>
2015-03-27 20:51:44 +0000

    robot img directive test beginnings

--- test/spec/directives/robot-img.js
+++ test/spec/directives/robot-img.js
@@ -20,9 +20,22 @@ describe('Directive: robotImg', function () {
     //mock request to html
     httpBackend.whenGET('my.html').respond('gotIt');
     scope = rootScope.$new();
-    template = compile('<div class="robot-img"><input type="text" ng-model="imageParam" placeholder="random characters"/><br/><img ng-show="imageParam" ng-src="http://robohash.org/{{imageParam}}" alt="robot image" /><br/><small ng-hide="imageParam">no robots yet...</small></div>')(scope);
+    template = compile('<input type="text" ng-model="imageParam" placeholder="random characters"/><br/><img id="testing-500" ng-show="imageParam" ng-src="http://robohash.org/{{imageParam}}" alt="robot image" /><br/><small ng-hide="imageParam">no robots yet...</small>')(scope);
     scope.$digest();
+
   }));
+
+  describe('initialisation', function() {
+
+    it('should have an ng-model', function() {
+      expect(template.attr('ng-model')).toEqual('imageParam');
+    });
+
+    it('should have the correct placeholder text', function() {
+      expect(template.attr('placeholder')).toEqual('random characters');
+    });
+
+  });
 
 
 });
