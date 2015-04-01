12c4f39d0f6319ae43b4e8d915a3bdaeb6395b14
ttbarnes <tony@tonybarnes.me>
2015-04-01 09:19:21 +0100

    simple service e2e tests

---
+++ test/e2e/spec/services/simple-service.js
@@ -0,0 +1,36 @@
+describe('page: services - simple service', function() {
+
+  beforeEach(function() {
+    browser.get('http://localhost:9000/#/services/simple-service');
+  });
+
+  it('should have a header title defined', function(){ 
+    var pageTitle = element.all(by.css('.page-title')).get(0);
+    expect(pageTitle).toBeDefined();
+    expect(pageTitle.getText()).toEqual('simple service');
+  });
+
+  var items = element.all(by.css('.simple-service li'));
+
+  it('should have 3 items defined', function(){
+    expect(items).toBeDefined();
+    expect(items.count()).toEqual(3);
+  });
+
+  describe('item specfics', function(){
+
+    it('should have the correct copy in the 1st item', function(){
+      expect((items).get(0).getText()).toEqual('Clark Kent');
+    });
+
+    it('should have the correct copy in the 2nd item', function(){
+      expect((items).get(1).getText()).toEqual('Lois Lane');
+    });
+
+    it('should have the correct copy in the 3rd item', function(){
+      expect((items).get(2).getText()).toEqual('Lex Luthor');
+    });
+
+  });
+
+});
