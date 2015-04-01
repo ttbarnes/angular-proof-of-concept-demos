11bb778aa1a562fd0ccafb67889396378c618c55
ttbarnes <tony@tonybarnes.me>
2015-04-01 22:10:06 +0100

    ng-repeat-filter-toggle e2e tests

--- test/e2e/spec/ng-repeat/ng-repeat-filter-toggle.js
+++ test/e2e/spec/ng-repeat/ng-repeat-filter-toggle.js
@@ -10,14 +10,51 @@ ddescribe('page: ng-repeat - ng-repeat filter toggle', function() {
     expect(pageTitle.getText()).toEqual('ng-repeat filter toggle');
   });
 
-  var filterToggleString = element.all(by.css('.task-filter-toggle.by-string li'));
-  var filterToggleNumeric = element.all(by.css('.task-filter-toggle.by-numeric li'));
+  var filterToggleString = element(by.css('.task-filter-toggle.by-string'));
+  var filterToggleStringTitle = element(by.css('.task-filter-toggle.by-string small'));
+  var filterToggleStringItems = element.all(by.css('.task-filter-toggle.by-string li'));
+
+  var filterToggleNumeric = element(by.css('.task-filter-toggle.by-numeric'));
+  var filterToggleNumericTitle = element(by.css('.task-filter-toggle.by-numeric small'));
+  var filterToggleNumericItems = element.all(by.css('.task-filter-toggle.by-numeric li'));
+
   var tasks = element.all(by.css('.task-item'));
 
   describe('initial view', function(){
-    it('should have 2 tasks defined', function(){
-      expect(tasks).toBeDefined();
-      expect(tasks.count()).toEqual(2);
+
+    it('should have a toggle by string list defined', function(){
+      expect(filterToggleString).toBeDefined();
+    });
+
+    it('should have a toggle by string title', function(){
+      expect(filterToggleStringTitle).toBeDefined();
+      expect(filterToggleStringTitle.getText()).toEqual('Filter by string (task.status):');
+    });
+
+    it('should have a toggle by string list with 3 items', function(){
+      expect(filterToggleStringItems.count()).toEqual(3);
+    });
+
+    it('should have a toggle by mumeric list defined', function(){
+      expect(filterToggleNumeric).toBeDefined();
+    });
+
+    it('should have a toggle by numeric title', function(){
+      expect(filterToggleNumericTitle).toBeDefined();
+      expect(filterToggleNumericTitle.getText()).toEqual('Filter by numeric (task.specialId):');
+    });
+
+    it('should have a toggle by numeric list with 3 items', function(){
+      expect(filterToggleNumericItems.count()).toEqual(3);
+    });
+
+    describe('2 initial tasks', function(){
+
+      it('should be defined', function(){
+        expect(tasks).toBeDefined();
+        expect(tasks.count()).toEqual(2);
+      });
+
     });
 
   });
