c00d53e834ff2eac2fc34644bd5dd325176cf5f6
ttbarnes <tony@tonybarnes.me>
2015-04-06 11:11:46 +0100

    active-checkbox-label - test initialisation

--- test/e2e/spec/forms/active-checkbox-label.js
+++ test/e2e/spec/forms/active-checkbox-label.js
@@ -10,17 +10,17 @@ ddescribe('page: forms - active-checkbox-label', function() {
     expect(pageTitle.getText()).toEqual('active checkbox label');
   });
 
-  /*
   var hasClass = function (element, cls) {
     return element.getAttribute('class').then(function (classes) {
       return classes.split(' ').indexOf(cls) !== -1;
     });
   };
-  */
-  
+
   var listItems = element.all(by.css('.active-checkbox-label-wrap li'));
-  //var listItemsInnerDiv = element.all(by.css('.active-checkbox-label-wrap li div'));
-  //var listItemsLabel = element.all(by.css('.active-checkbox-label-wrap li label'));
+  var listItemsInnerDiv = element.all(by.css('.active-checkbox-label-wrap .inner'));
+  var listItemsLabel = element.all(by.css('.active-checkbox-label-wrap li label'));
+  var listItemsInput = element.all(by.css('.active-checkbox-label-wrap li input'));
+  var listItemsDesc = element.all(by.css('.active-checkbox-label-wrap li small'));
 
   describe('initialisation', function(){
 
@@ -28,21 +28,29 @@ ddescribe('page: forms - active-checkbox-label', function() {
       expect(listItems.count()).toEqual(3);
     });
 
-    /*
-    it('should have 3 items with label copy', function(){
-      expect(listItemsLabel).toBeDefined();
-    });
-    */
-
-
-    /*
     it('should not have a checked class', function(){
-      expect(hasClass(element(by.css(listItemsInnerDiv)), 'checked')).toBe(false);
+      expect(hasClass(element(by.css('.active-checkbox-label-wrap .inner')), 'checked')).toBe(false);
     });
-    */
+
+    it('should have an isChecked ng-model', function(){
+      expect(listItemsInput.get(0).getAttribute('ng-model')).toEqual('isChecked');
+      expect(listItemsInput.get(1).getAttribute('ng-model')).toEqual('isChecked');
+      expect(listItemsInput.get(2).getAttribute('ng-model')).toEqual('isChecked');
+    });
+
+    it('should have label copy', function(){
+      expect(listItemsLabel).toBeDefined();
+      expect(listItemsLabel.count()).toEqual(3);
+      expect(listItemsLabel.getText()).toBeDefined();
+    });
+
+    it('should have description copy', function(){
+      expect(listItemsDesc).toBeDefined();
+      expect(listItemsDesc.count()).toEqual(3);
+      expect(listItemsDesc.getText()).toBeDefined();
+    });
 
   });
-
 
 
 });
