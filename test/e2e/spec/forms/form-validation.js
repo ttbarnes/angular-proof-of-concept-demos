b7c36d6a2d543966039668511068c09c4adab43b
ttbarnes <tony@tonybarnes.me>
2015-04-08 17:15:39 +0100

    form validation - favourite url tests

--- test/e2e/spec/forms/form-validation.js
+++ test/e2e/spec/forms/form-validation.js
@@ -254,6 +254,51 @@ ddescribe('page: forms - form-validation', function() {
 
   });
 
+describe('favourite url', function(){
+
+    var label = element(by.css('.row.favourite-url label'));
+    var input = element(by.css('.row.favourite-url input'));
+    var error = element(by.css('.row.favourite-url span.error'));
+
+    it('should have the correct html/angular label attribute', function(){
+      expect(label.getAttribute('for')).toEqual('favouriteUrl');
+    });
+
+    it('should have the correct html/angular input attributes', function(){
+      expect(input.getAttribute('type')).toEqual('url');
+      expect(input.getAttribute('ng-model')).toEqual('favouriteUrl');
+      expect(input.getAttribute('required')).toBeDefined();
+    });
+
+    it('should have the correct ng-show values in the error message', function(){
+      expect(error.getAttribute('ng-show')).toEqual('!signupForm.favouriteUrl.$pristine && signupForm.favouriteUrl.$invalid');
+    });
+
+    it('should have error message hidden by default', function(){
+      expect(error.isDisplayed()).toBeFalsy();
+    });
+
+    it('should display error message when an incorrect url entered', function(){
+      input.sendKeys('www.google.com');
+      expect(error.isDisplayed()).toBeTruthy();
+      input.clear();
+      input.sendKeys('google.com');
+      expect(error.isDisplayed()).toBeTruthy();
+    });
+
+    it('should display error message if you add a url and then remove', function(){
+      input.sendKeys('http://google.com');
+      input.clear();
+      expect(error.isDisplayed()).toBeTruthy();
+    });
+
+    it('should display the correct error message copy', function(){
+      input.sendKeys('google');
+      expect(error.getText()).toEqual('This url needs to contain \'http://\'.');
+    });
+
+  });
+
 });
 
 
