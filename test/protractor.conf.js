b7cf986ad60ff822f6c641a7bded63543a0f8925
ttbarnes <tony@tonybarnes.me>
2015-04-12 15:43:12 +0100

    split e2e specs, reduce test time by 27s

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,16 +1,51 @@
 exports.config = {
+  multiCapabilities: [
+    {
+        shardTestFiles: false,
+        maxInstances: 1,
+        browserName: 'chrome',
+        specs: ['e2e/spec/home.js']
+    },
+    {
+        shardTestFiles: false,
+        maxInstances: 1,
+        browserName: 'chrome',
+        specs: ['e2e/spec/directives/*.js']
+    },
+    {
+        shardTestFiles: false,
+        maxInstances: 1,
+        browserName: 'chrome',
+        specs: ['e2e/spec/forms/*.js']
+    },
+    {
+        shardTestFiles: false,
+        maxInstances: 1,
+        browserName: 'chrome',
+        specs: ['e2e/spec/ng-repeat/*.js']
+    },
+    {
+        shardTestFiles: false,
+        maxInstances: 1,
+        browserName: 'chrome',
+        specs: ['e2e/spec/services/*.js']
+    },
+    {
+        shardTestFiles: false,
+        maxInstances: 1,
+        browserName: 'chrome',
+        specs: ['e2e/spec/ui/*.js']
+    }
+  ],
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['e2e/spec/{,*/}*.js'],
+  //specs: ['e2e/spec/{,*/}*.js'],
   baseUrl: 'http://localhost:9001',
-
   onPrepare: function() {
-      var SpecReporter = require('jasmine-spec-reporter');
-      jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: true}));
+    var SpecReporter = require('jasmine-spec-reporter');
+    jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: true}));
 
-      var width = 800;
-      var height = 600;
-      browser.driver.manage().window().setSize(width, height);
-
-   }
-
+    var width = 800;
+    var height = 600;
+    browser.driver.manage().window().setSize(width, height);
+  }
 }
