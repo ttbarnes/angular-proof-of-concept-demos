33c9700e2b25de2c9f9ba2c4ee77f8c71f59c5b4
ttbarnes <tony@tonybarnes.me>
2015-04-26 14:32:42 +0100

    active hover directive e2e

--- test/e2e/spec/directives/active-hover.js
+++ test/e2e/spec/directives/active-hover.js
@@ -1,13 +1,13 @@
-describe('page: ui - ng-click show/hide', function() {
+describe('page: directives - active-hover', function() {
 
   beforeEach(function() {
-    browser.get('http://localhost:9000/#/ui/ng-click-show-hide');
+    browser.get('http://localhost:9000/#/directives/active-hover');
   });
 
   it('should have a header title defined', function(){ 
     var pageTitle = element.all(by.css('.page-title')).get(0);
     expect(pageTitle).toBeDefined();
-    expect(pageTitle.getText()).toEqual('ng-click show/hide');
+    expect(pageTitle.getText()).toEqual('active hover');
   });
 
   var hasClass = function (element, cls) {
@@ -16,48 +16,59 @@ describe('page: ui - ng-click show/hide', function() {
     });
   };
 
-  var button = element(by.css('.ng-click-show-hide-wrapper button'));
-  var mainMessage = element(by.css('.ng-click-show-hide-wrapper p'));
+  var items = element.all(by.css('.active-hover-items div'));
+  var item1 = items.get(0);
+  var item2 = items.get(1);
+  var item3 = items.get(2);
+  var item4 = items.get(3);
 
   describe('initialisation', function(){
 
-    it('should have the correct ng-click expression', function(){
-      expect(button.getAttribute('ng-click')).toEqual('showSomething = !showSomething');
+    it('should have 4 items', function(){
+      expect(items.count()).toEqual(4);
     });
 
-    it('should have the correct ng-show expression', function(){
-      expect(mainMessage.getAttribute('ng-show')).toEqual('showSomething');
+    it('should have 4 items with active-hover attribute', function(){
+      expect(item1.getAttribute('active-hover')).toBeDefined();
+      expect(item2.getAttribute('active-hover')).toBeDefined();
+      expect(item3.getAttribute('active-hover')).toBeDefined();
+      expect(item4.getAttribute('active-hover')).toBeDefined();
     });
 
-    it('should have the correct copy in button', function(){
-      expect(button.getText()).toEqual('show me something');
+    it('should have 4 items with no class', function(){
+      expect(hasClass(item1, 'active-hover')).toBe(false);
+      expect(hasClass(item1, 'inactive-hover')).toBe(false);
+      expect(hasClass(item2, 'active-hover')).toBe(false);
+      expect(hasClass(item2, 'inactive-hover')).toBe(false);
+      expect(hasClass(item3, 'active-hover')).toBe(false);
+      expect(hasClass(item3, 'inactive-hover')).toBe(false);
+      expect(hasClass(item4, 'active-hover')).toBe(false);
+      expect(hasClass(item4, 'inactive-hover')).toBe(false);
     });
-
-    it('should have the main message hidden', function(){
-      expect(mainMessage.isDisplayed()).toBeFalsy();
-    });
-
+    
   });
 
   describe('interaction', function(){
 
-    it('should show and hide main message when clicking', function(){
-      button.click();
-      expect(mainMessage.isDisplayed()).toBeTruthy();
-      button.click();
-      expect(mainMessage.isDisplayed()).toBeFalsy();
-      button.click();
-      expect(mainMessage.isDisplayed()).toBeTruthy();
-      button.click();
-      expect(mainMessage.isDisplayed()).toBeFalsy();
+    it('should add correct active/inactive classes on mouse over', function(){
+      browser.actions().mouseMove(item1).perform();
+      expect(item1.getAttribute('class')).toEqual('active-hover');
+      expect(item2.getAttribute('class')).toEqual('inactive-hover');
     });
 
-    it('should have the correct copy in the main message', function(){
-      button.click();
-      expect(mainMessage.getText()).toEqual('hello! Is it me you\'re looking for?');
+    it('should add correct active/inactive classes on multiple mouse overs', function(){
+      browser.actions().mouseMove(item2).perform();
+      expect(item2.getAttribute('class')).toEqual('active-hover');
+      expect(item1.getAttribute('class')).toEqual('inactive-hover');
+      expect(item3.getAttribute('class')).toEqual('inactive-hover');
+      expect(item4.getAttribute('class')).toEqual('inactive-hover');
+      browser.actions().mouseMove(item3).perform();
+      expect(item3.getAttribute('class')).toEqual('active-hover');
+      expect(item1.getAttribute('class')).toEqual('inactive-hover');
+      expect(item2.getAttribute('class')).toEqual('inactive-hover');
+      expect(item4.getAttribute('class')).toEqual('inactive-hover');
     });
 
   });
-
 
 });
