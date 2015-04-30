0f8a8bf99a8a0d82b5a6eacda1ecace7df9a00b9
ttbarnes <tony@tonybarnes.me>
2015-04-30 14:25:44 +0100

    robot image initilisation e2e tests

--- test/e2e/spec/directives/robot-img.js
+++ test/e2e/spec/directives/robot-img.js
@@ -10,10 +10,13 @@ describe('page: directives - random-robot-image', function() {
     expect(pageTitle.getText()).toEqual('random robot image');
   });
 
-  var wrapperElm = '.robot-img-wrap';
+  var wrapperElm = '.robot-img-wrap ';
+  var robotImgWrapper = 'div.robot-img ';
   var wrapper = element(by.css(wrapperElm));
   var introText = element(by.css(wrapperElm + 'p'));
-  var robotImgWrapper = element(by.css(wrapperElm + 'div.robot-img'));
+  var textInput = element(by.css(robotImgWrapper + 'input'));
+  var img = element(by.css(robotImgWrapper + 'img'));
+  var noRobotsText = element(by.css(robotImgWrapper + 'small'));
 
   describe('initialisation', function(){
 
@@ -21,8 +24,51 @@ describe('page: directives - random-robot-image', function() {
       expect(introText.getText()).toEqual('enter some random characters to generate a unique robot:');
     });
 
-    it('should have robot-img div element', function(){
+    it('should have a div', function(){
       expect(robotImgWrapper).toBeDefined();
+    });
+
+    describe('text input', function(){
+
+      it('should have a text input defined', function(){
+        expect(textInput).toBeDefined();
+      });
+
+      it('should have a text input with the correct attributes', function(){
+        expect(element(by.css('.robot-img-wrap input')).getAttribute('type')).toEqual('text');
+        expect(element(by.css('.robot-img-wrap input')).getAttribute('ng-model')).toEqual('imageParam');
+        expect(element(by.css('.robot-img-wrap input')).getAttribute('placeholder')).toEqual('random characters');
+      });
+
+    });
+
+    describe('image', function(){
+
+      it('should be defined', function(){
+        expect(img).toBeDefined();
+      });
+
+      it('should have a robohash url and empty param', function(){
+        expect(img.getAttribute('ng-src')).toEqual('http://robohash.org/');
+      });
+
+      it('should have correct alt copy', function(){
+        expect(img.getAttribute('alt')).toEqual('robot image');
+      });
+
+    });
+
+    describe('no robots text', function(){
+
+      it('should be defined and visible', function(){
+        expect(noRobotsText).toBeDefined();
+        expect(noRobotsText.isDisplayed()).toBeTruthy();
+      });
+
+      it('should have the correct copy', function(){
+        expect(noRobotsText.getText()).toEqual('no robots yet...');
+      });
+
     });
 
   });
